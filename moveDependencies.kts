14615e62703119304582c16345f605a3606e07f7
NicholasPowell <nicholas.powell@gmail.com>
2022-12-18 10:27:55 -0500

    Create moveDependencies.kts

---
+++ moveDependencies.kts
@@ -0,0 +1,21 @@
+#!/usr/bin/env kscript
+@file:Include("files.kt")
+@file:Include("gradleBuildTemplate.kt")
+
+import java.io.File
+
+val dependsOnMaven = "@file:DependsOnMaven\\((.*)\\)".toRegex()
+
+@OptIn(ExperimentalStdlibApi::class)
+val dependencies = scriptFiles
+    .toList()
+    .map { it.readLines() }
+    .flatMap { lines ->
+        lines.filter { line -> line.startsWith("@file:DependsOnMaven(") }
+    }
+    .toSet()
+    .map {dependsOnMaven.matchAt(it,0)!!.groupValues[1] }
+    .map { "implementation($it)"}
+
+File("../build.gradle.kts")
+    .writeText(gradleBuildTemplate(dependencies))
