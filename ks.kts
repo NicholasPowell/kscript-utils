21a5d848703987e64583a36598f25ec5a089661b
NicholasPowell <nicholas.powell@gmail.com>
2022-12-19 07:59:18 -0500

    Update ks.kts

--- ks.kts
+++ ks.kts
@@ -1,9 +1,13 @@
 #!/usr/bin/env kscript
 
 @file:Include("runCommand.kts")
+@file:DependsOnMaven("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
 val kst = "kscript"
 val gistUrl = "https://raw.githubusercontent.com/NicholasPowell/kscript-utils/main/"
 
-fun ks(action: String) = executeShellCommand(kst, gistUrl + action)
+fun ks(action: String) = 
+  runBlocking {
+    executeShellCommand(kst, gistUrl + action)
+  }
 
