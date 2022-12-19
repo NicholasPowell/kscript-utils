a341213ed94ff3ebd590149eaf4a09fd982023ab
NicholasPowell <nicholas.powell@gmail.com>
2022-12-19 07:54:02 -0500

    Update ks.kts

--- ks.kts
+++ ks.kts
@@ -5,5 +5,5 @@
 val kst = "kscript"
 val gistUrl = "https://raw.githubusercontent.com/NicholasPowell/kscript-utils/main/"
 
-fun ks(action: String) = runShellCommand(kst, gistUrl + action)
+fun ks(action: String) = executeShellCommand(kst, gistUrl + action)
 
